0b1d17ed51024939ce236b9cba1094327f10de05
SimonHotDog <simon.lu@live.se>
2022-04-11 18:39:53 +0200

    Started on chapter 9

---
+++ exercises/exercises-ch9.hs
@@ -0,0 +1,27 @@
+
+-- 9.2
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use const" #-}
+
+length :: [t] -> Int
+length list = sum (map (\n -> 1) list)
+
+-- 9.6
+
+asSquares :: [Int] -> [Int]
+asSquares = map (^ 2)
+
+sumSquares :: [Int] -> Int
+sumSquares ns = sum (asSquares ns)
+
+greaterThanZero :: [Int] -> Bool
+greaterThanZero = all (> 0)
+
+-- 9.7
+
+minf :: (Int -> Int) -> Int -> Int 
+minf f 0 = f 0
+minf f n = min (f n) (minf f (n-1))
+
+--valeqf :: (Int -> Int) -> Int -> Bool 
+
