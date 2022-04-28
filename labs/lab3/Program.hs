d510a1e14bbe30c7fbb4f074fdf5955dc0626b46
SimonHotDog <simon.lu@live.se>
2022-04-28 14:32:20 +0200

    Program.hs finished

--- labs/lab3/Program.hs
+++ labs/lab3/Program.hs
@@ -1,11 +1,23 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Program(T, parse, fromString, toString, exec) where
 import Parser hiding (T)
 import qualified Statement
 import qualified Dictionary
 import Prelude hiding (return, fail)
-newtype T = Program () -- to be defined
+
+
+newtype T = Program [Statement.T]
+
+program :: Parser T
+program = iter (spaces -# Statement.parse #- spaces) >-> Program
+
+str :: T -> String
+str (Program (s : stmts)) = toString s ++ "\n" ++ str (Program stmts)
+str (Program []) = ""
+
 instance Parse T where
-  parse = error "Program.parse not implemented"
-  toString = error "Program.toString not implemented"
-             
-exec = error "Program.exec not implemented"
+  parse = program
+  toString = str
+
+exec :: Program.T -> [Integer] -> [Integer]
+exec (Program stmts) = Statement.exec stmts Dictionary.empty
